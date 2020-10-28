d04d3f933311111a22c7920e7fcf3446c73ee083
Raj <rajkamal11126@gmail.com>
2020-10-29 00:02:19 +0530

    Added : Delete the element from the LinkedList

--- src/main/java/com/bridgelabz/MyNode/MyLinkedList.java
+++ src/main/java/com/bridgelabz/MyNode/MyLinkedList.java
@@ -2,48 +2,52 @@ package com.bridgelabz.MyNode;
 
 public class MyLinkedList<K> {
 	public INode<K> head;
-	public INode<K> tail; 
-	
+	public INode<K> tail;
+
 	public MyLinkedList() {
 		this.head = null;
 		this.tail = null;
 	}
+
 	public void insert(INode myNode, INode newNode) {
 		INode tempNode = myNode.getNext();
 		myNode.setNext(newNode);
-		newNode.setNext(tempNode);	
+		newNode.setNext(tempNode);
 	}
-	public INode pop() {
+
+	public INode delete() {
 		INode tempNode = this.head;
-		this.head = head.getNext();
+		this.head = this.head.getNext();
 		return tempNode;
 	}
+
 	public void add(INode<K> newNode) {
 		if (this.tail == null) {
 			this.tail = newNode;
 		}
 		if (this.head == null) {
 			this.head = newNode;
-		}
-		else {
+		} else {
 			INode<K> tempNode = this.head;
 			this.head = newNode;
 			this.head.setNext(tempNode);
 		}
 	}
-	//append the nodes 
+
+	// append the nodes
 	public void append(MyNode newNode) {
-		if(this.head == null )
+		if (this.head == null)
 			this.head = newNode;
-		if(this.tail == null)
+		if (this.tail == null)
 			this.tail = newNode;
 		else {
 			this.tail.setNext(newNode);
-			this.tail = newNode;	
-		}		
+			this.tail = newNode;
+		}
 	}
-	//print the nodes
+
+	// print the nodes
 	public void printMyNodes() {
-		System.out.println("My Nodes: "+head);
+		System.out.println("My Nodes: " + head);
 	}
 }
--- src/test/java/com/bridgelabz/MyNodeTest/MyLinkedListTest.java
+++ src/test/java/com/bridgelabz/MyNodeTest/MyLinkedListTest.java
@@ -19,7 +19,7 @@ public class MyLinkedListTest {
 	}
 
 	@Test
-	public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop() {
+	public void given3NumbersWhenInsertToLinkedListShouldBeAddedToTop() {
 		MyNode<Integer> myFirstNode = new MyNode<>(70);
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
 		MyNode<Integer> myThirdNode = new MyNode<>(56);
@@ -34,7 +34,7 @@ public class MyLinkedListTest {
 	}
 
 	@Test
-	public void given3NumbersWhenAddedToLinkedListShouldBeAddedToLast() {
+	public void given3NumbersWhenInsertingSecondInBetweenShouldPassLinkedListResult() {
 		MyNode<Integer> myFirstNode = new MyNode<>(70);
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
 		MyNode<Integer> myThirdNode = new MyNode<>(56);
@@ -45,6 +45,35 @@ public class MyLinkedListTest {
 		myLinkedList.printMyNodes();
 		boolean result = myLinkedList.tail.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode)
 				&& myLinkedList.head.equals(myFirstNode);
+		Assert.assertTrue(result);
+	}
+	@Test
+	public void given3NumbersWhenInsertingInBetweenLinkedShouldPassLinkedListResult() {
+		MyNode<Integer> myFirstNode = new MyNode<>(70);	
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(56);
+		MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+		myLinkedList.add(myFirstNode);
+		myLinkedList.append(myThirdNode);
+		myLinkedList.insert(myFirstNode ,mySecondNode);
+		myLinkedList.printMyNodes();
+		boolean result = myLinkedList.tail.equals(myThirdNode) &&
+				 myLinkedList.head.getNext().equals(mySecondNode) &&
+				 myLinkedList.head.equals(myFirstNode);
+		Assert.assertTrue(result);
+	}
+	@Test
+	public void given3NumbersWhenFirstElementDeletedShouldPassLinkedListResult() {
+		MyNode<Integer> myFirstNode = new MyNode<>(70);	
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(56);
+		MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+		myLinkedList.add(myFirstNode);
+		myLinkedList.add(mySecondNode);
+		myLinkedList.add(myThirdNode);
+		myLinkedList.delete();
+		myLinkedList.printMyNodes();
+		boolean result = myLinkedList.head.equals(mySecondNode);
 		Assert.assertTrue(result);
 	}
 }
